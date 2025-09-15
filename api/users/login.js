f41f18f6a24859dfe501de3f8be2a0fa640cdca9
sooryashibu <36664818+sooryashibu@users.noreply.github.com>
2025-09-16 01:56:11 +0200

    Update login.js

--- api/users/login.js
+++ api/users/login.js
@@ -4,19 +4,6 @@ import User from '../../models/User.js';
 import jwt from 'jsonwebtoken';
 
 export default async function handler(req, res) {
-  // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
-
   try {
     await dbConnect();
 
