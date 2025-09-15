bb68eba9f9e64c15bb5e9f43acbc3a58ba69edec
sooryashibu <36664818+sooryashibu@users.noreply.github.com>
2025-09-16 01:44:10 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,25 +1,45 @@
-import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+// models/User.js
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-// Compare password
+// Check password during login
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+// Backwards-compatible alias (some files use comparePassword)
+userSchema.methods.comparePassword = userSchema.methods.matchPassword;
 
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 export default User;
